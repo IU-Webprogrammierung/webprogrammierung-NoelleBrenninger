f0ead0eed1fc76b1cff421cab1ef387cb6ad8d0e
Noelle Brenninger <noelle.brenninger@iu-study.org>
2026-02-13 12:22:10 +0100

    refactor footer navigation, remove Datenschutz page, update Impressum content, add animation aria-expanded true & false for accordeon

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 $("#page-header").load("components/header.component");
 $("#footer").load("components/footer.component");
+$('.faq-accordeon button').on('click', function () {
+    const $clickedButton = $(this);
+    const targetId = $clickedButton.attr('aria-controls');
+    const $targetAnswer = $('#' + targetId);
+    const isCurrentlyExpanded = $clickedButton.attr('aria-expanded') === 'true';
+
+    $('.faq-accordeon button[aria-expanded="true"]').not($clickedButton).each(function () {
+        const $otherButton = $(this);
+        const otherTargetId = $otherButton.attr('aria-controls');
+        const $otherAnswer = $('#' + otherTargetId);
+        $otherButton.attr('aria-expanded', 'false');
+        $otherAnswer.slideUp(300);
+    });
+    if (isCurrentlyExpanded) {
+        $clickedButton.attr('aria-expanded', 'false');
+        $targetAnswer.slideUp(300);
+    } else {
+        $clickedButton.attr('aria-expanded', 'true');
+        $targetAnswer.slideDown(300);
+    }
+});
